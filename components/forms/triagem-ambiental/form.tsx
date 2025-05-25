2572ebd675204467d6ef5ee59d8603665fd01404
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-05-25 11:23:21 +0200

    Fixed Resultado triagem plus button disabled

--- components/forms/triagem-ambiental/form.tsx
+++ components/forms/triagem-ambiental/form.tsx
@@ -709,7 +709,6 @@ export function TriagemAmbientalForm({
                     <AddOptionDialog
                       type='Resultado'
                       onAdd={(value) => handleAddNewOption('resultado', value)}
-                      disabled={!form.getValues('subprojectoId')}
                     />
                   </div>
                   <FormMessage />
